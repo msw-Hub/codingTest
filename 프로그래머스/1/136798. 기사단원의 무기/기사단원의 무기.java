33a2f826e762e69bec719c7fd45011d8e468006e
msw-Hub <133941998+msw-Hub@users.noreply.github.com>
2024-06-22 20:56:30 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.64 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int[] num = new int[number + 1];
+        int result = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int sum = 0;
+            for (int j = 1; j * j <= i; j++) {
+                if (i % j == 0) {
+                    sum++;
+                    if (j != i / j) {
+                        sum++;
+                    }
+                }
+            }
+            if (sum > limit) {
+                num[i] = power;
+            } else {
+                num[i] = sum;
+            }
+            result += num[i];
+        }
+        return result;
+    }
+}
